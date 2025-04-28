6e5e2954d7db6df9aaf88ab504ea526511911375
balatuluva <bala.tulva@gmail.com>
2025-04-28 12:29:06 +0100

    RDS VPC setup

--- main.tf
+++ main.tf
@@ -52,3 +52,80 @@ resource "aws_subnet" "RDS-VPC-Private-Subnet" {
   }
 }
 
+resource "aws_route_table" "RDS-VPC-Public-RT" {
+  vpc_id = aws_vpc.RDS-VPC.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.RDS-IGW.id
+  }
+
+  tags = {
+    Name = "RDS-VPC-Public-RT"
+  }
+}
+
+resource "aws_route_table" "RDS-VPC-Private-RT" {
+  vpc_id = aws_vpc.RDS-VPC.id
+
+  tags = {
+    Name = "RDS-VPC-Private-RT"
+  }
+}
+
+resource "aws_route_table_association" "RDS-VPC-Public-RT-Association" {
+  count = 3
+  subnet_id = aws_subnet.RDS-VPC-Public-Subnet[count.index].id
+  route_table_id = aws_route_table.RDS-VPC-Public-RT.id
+}
+
+resource "aws_route_table_association" "RDS-VPC-Private-RT-Association" {
+  count = 3
+  subnet_id = aws_subnet.RDS-VPC-Private-Subnet[count.index].id
+  route_table_id = aws_route_table.RDS-VPC-Private-RT.id
+}
+
+resource "aws_security_group" "RDS-VPC-SG" {
+  name = "RDS-VPC-SG"
+  description = "Allow all outbound traffic but only allow ec2 and RDS inbound traffic"
+  vpc_id = aws_vpc.RDS-VPC.id
+
+  ingress {
+    description = "Allow HTTP"
+    protocol = "tcp"
+    from_port = 80
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow HTTPS"
+    protocol = "tcp"
+    from_port = 443
+    to_port = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow SSH"
+    protocol = "tcp"
+    from_port = 22
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow MySQL/Aurora from VPC Public Subnets"
+    protocol = "tcp"
+    from_port = 3306
+    to_port = 3306
+    cidr_blocks = ["192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24"]
+  }
+
+  egress {
+    from_port = "0"
+    to_port   = "0"
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "RDS-VPC-SG"
+  }
+}
