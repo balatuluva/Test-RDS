780d7361b35ccb4c808ce80d108c317f1b81502b
balatuluva <bala.tulva@gmail.com>
2025-04-29 06:08:50 +0100

    revert back

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
-output "rds_vpc_security_group_id" {
-  description = "The ID of the RDS VPC Security Group"
-  value       = aws_security_group.RDS-VPC-SG.id
-}
-
-output "rds_ec2_instance_id" {
-  description = "The ID of the RDS EC2 instance"
-  value       = aws_instance.RDS-EC2.id
-}
-
-output "rds_ec2_public_ip" {
-  description = "The public IP of the RDS EC2 instance"
-  value       = aws_instance.RDS-EC2.public_ip
-}
+#output "rds_vpc_security_group_id" {
+#  description = "The ID of the RDS VPC Security Group"
+#  value       = aws_security_group.RDS-VPC-SG.id
+#}
+#
+#output "rds_ec2_instance_id" {
+#  description = "The ID of the RDS EC2 instance"
+#  value       = aws_instance.RDS-EC2.id
+#}
+#
+#output "rds_ec2_public_ip" {
+#  description = "The public IP of the RDS EC2 instance"
+#  value       = aws_instance.RDS-EC2.public_ip
+#}
